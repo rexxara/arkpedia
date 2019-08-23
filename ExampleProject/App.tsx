46b707a31e36e39ea44f59ba287b46303351305a
rrrrrrrrrr <leiyiming@xiaomi.com>
2019-08-23 18:54:42 +0800

    rrrrrr

--- ExampleProject/App.tsx
+++ ExampleProject/App.tsx
@@ -1,59 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
-import React from 'react'
-import { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+/* tslint:disable*/
+import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { Tabs } from '@ant-design/react-native';
+const renderContent = (tab, index) => {
+  const style = {
+    paddingVertical: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    backgroundColor: '#ddd',
+  };
+  const content = [1, 2, 3, 4, 5, 6, 7, 8].map(i => {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View key={`${index}_${i}`} style={style}>
+        <Text>
+          {tab.title} - {i}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      </View>
+    );
+  });
+  return <ScrollView style={{ backgroundColor: '#fff' }}>{content}</ScrollView>;
+};
+export default class BasicTabsExample extends React.Component {
+  render() {
+    const tabs = [
+      { title: 'First Tab' },
+      { title: 'Second Tab' },
+      { title: 'Third Tab' },
+    ];
+    const tabs2 = [
+      { title: '1st Tab' },
+      { title: '2nd Tab' },
+      { title: '3rd Tab' },
+      { title: '4th Tab' },
+      { title: '5th Tab' },
+      { title: '6th Tab' },
+      { title: '7th Tab' },
+      { title: '8th Tab' },
+      { title: '9th Tab' },
+    ];
+    const style = {
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 150,
+      backgroundColor: '#fff',
+    };
+    return (
+      <View style={{ flex: 1 }}>
+        <Tabs tabs={tabs}>
+          <View style={style}>
+            <Text>Content of First Tab</Text>
+          </View>
+          <View style={style}>
+            <Text>Content of Second Tab</Text>
+          </View>
+          <View style={style}>
+            <Text>Content of Third Tab</Text>
+          </View>
+        </Tabs>
+        <View style={{ flex: 2 }}>
+          <Tabs tabs={tabs2} initialPage={1} tabBarPosition="top">
+            {renderContent}
+          </Tabs>
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export const title = 'Tabs';
+export const description = 'Tabs example';
